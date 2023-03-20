e2b276c3422f820f566d67fe9bfb11236f195241
Seungje Mun <34952598+JasonMoon95@users.noreply.github.com>
2023-03-20 22:26:45 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,23 +3,9 @@
 REPOSITORY=/home/ec2-user/app/step1
 PROJECT_NAME=Haribo
 
-cd $REPOSITORY/$PROJECT_NAME/
-
-echo "> Git pull"
-
-git pull
-
-echo "> 프로젝트 build 시작"
-
-./gradlew build
-
-echo "> step1 디렉토리 이동"
-
-cd $REPOSITORY
-
 echo "> Build 파일 복사"
 
-cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+cp /home/ec2-user/build/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 
